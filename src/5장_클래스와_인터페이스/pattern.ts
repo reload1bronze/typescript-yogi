26c1df2dfa843e223ef247da9f4beb89d28ceaf5
reload1bronze <nature9th@gmail.com>
2022-02-04 14:01:31 +0900

    TS 5장 클래스와 인터페이스 - 빌더 패턴

---
+++ src/5장_클래스와_인터페이스/pattern.ts
@@ -0,0 +1,29 @@
+// 5.11.1 팩토리 패턴
+interface Shoe {
+  purpose: string;
+}
+
+class BalletFlat implements Shoe {
+  purpose = "dancing";
+}
+
+class Boot implements Shoe {
+  purpose = "woodcutting";
+}
+
+class Sneaker implements Shoe {
+  purpose = "walking";
+}
+
+let Shoe = {
+  create(type: "balletFlat" | "boot" | "sneaker"): Shoe {
+    switch (type) {
+      case "balletFlat":
+        return new BalletFlat();
+      case "boot":
+        return new Boot();
+      case "sneaker":
+        return new Sneaker();
+    }
+  },
+};
